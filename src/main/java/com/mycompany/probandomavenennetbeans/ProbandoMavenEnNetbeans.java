18ffe5725134f4db86f490dc5c80f04f5181d3c6
sturniolo <br1.sturniolo@gmail.com>
2023-09-30 15:40:29 -0300

    subiendo proyecto copia

---
+++ src/main/java/com/mycompany/probandomavenennetbeans/ProbandoMavenEnNetbeans.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.probandomavenennetbeans;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author br1st
+ */
+public class ProbandoMavenEnNetbeans {
+
+    public static void main(String[] args) {
+        Connection con;
+        con = Conexion.getConexion();
+    }
+}
